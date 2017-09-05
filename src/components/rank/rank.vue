435cf6d8d3632089c7bba2da5038115163ef191f
dingzhong <dingzhong55555@gmail.com>
2017-09-05 18:02:11 +0800

    compelet header component

---
+++ src/components/rank/rank.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="rank">
+    <span>排行</span>
+  </div>
+</template>
+
+<script type="text/ecmascript-6">
+export default {
+}
+</script>
+
+<style scoped lang="stylus" rel="stylesheet/stylus">
+
+</style>
---
+++ src/components/recommend/recommend.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="recommend">
+    <span>歌曲推荐</span>
+  </div>
+</template>
+
+<script type="text/ecmascript-6">
+export default {
+}
+</script>
+
+<style scoped lang="stylus" rel="stylesheet/stylus">
+
+</style>
